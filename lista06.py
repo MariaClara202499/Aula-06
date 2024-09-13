55eb17a1473de8e412d6bf5353ae19d65f4d3926
MariaClara202499 <schoolmariaclara@gmail.com>
2024-09-13 15:07:25 -0300

    Add files via upload

--- lista06.py
+++ lista06.py
@@ -1,3 +1,6 @@
-# Criando conjunto - Aluna Maria Clara Ferreira Martins 1B
+# Criando conjunto - Aluno Julio Martins 1B
 alunos = {"Ana","Bruno","Carla","Daniel"}
+print(alunos)
+
+alunos.add("Eduarda")
 print(alunos)
